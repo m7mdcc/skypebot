c57d0c67b6f3eb97742d5eaf5abf417d88b902a5
Mike Simons <msimons@inviqa.com>
2013-09-24 10:33:34 +0200

    Adds message filter to jenkins endpoint to allow builds to only report (for instance) failed builds

--- jenkins.php
+++ jenkins.php
@@ -4,6 +4,18 @@ require_once 'vendor/autoload.php';
 $payload = json_decode(file_get_contents('php://input'));
 $info = "";
 
+if(isset($_REQUEST['filter']) ) {
+    $match = match_filters(
+        (array) $_REQUEST['filter'],
+        $_REQUEST,
+        $payload
+    );
+
+    if($match !== true) {
+        die($match);
+    }
+}
+
 switch($payload->build->phase) {
     case 'STARTED':
         $info = sprintf(
@@ -27,3 +39,51 @@ switch($payload->build->phase) {
 }
 
 Inviqa\SkypeEngine::getDbusProxy()->Invoke( "CHATMESSAGE {$_REQUEST['id']} $info");
+
+/**
+ * Determines if requested filters mach payload.
+ *
+ * Nested fields can be accessed by using a hyphen.
+ * e.g. for payload { build: {status: success } }
+ * build status can be filtered with "build-status"
+ *
+ * @param mixed $filters String (single field) or array of fields to filter on.
+ * @param array $input Input that specifies filter values.
+ * @param stdObject $payload Parsed payload.
+ *
+ * @return mixed Boolean true if filters are OK, string with failure reason otherwise.
+ */
+function match_filters($filters, $input, $payload) {
+    foreach($filters as $filter) {
+        $key = "filter-{$filter}";
+        if(isset($input[$key])) {
+            $value = get_payload_value($filter, $payload);
+            if($input[$key] != $value) {
+                return "'$filter' filter did not match ('{$input[$key]}' != '$value')\n";
+            }
+        }
+    }
+    return true;
+}
+
+/**
+ * Traverses the payload and returns the value specified by path.
+ *
+ * Nesting is represented by a hyphen.
+ *
+ * @param string $path Path to field
+ * @param stdobject $payload Parsed payload
+ * @return mixed Null if path could not be accessed, value of field specified by path otherwise
+ */
+function get_payload_value($path, $payload) {
+    $current = $payload;
+    $path = explode('-', $path);
+    foreach($path as $elem) {
+        if(is_object($current) && isset($current->{$elem})) {
+            $current =& $current->{$elem};
+        } else {
+            return null;
+        }
+    }
+    return $current;
+}
