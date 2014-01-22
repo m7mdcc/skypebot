01e3ddb7e6d0d90286e7d1c6d8d026c72c2b8a52
Shane Auckland <shane.auckland@gmail.com>
2014-01-22 12:40:02 +0000

    Add initial VM with Skype provisioned

---
+++ tools/chef/app-cookbooks/ubuntu-config/recipes/default.rb
@@ -0,0 +1,9 @@
+execute 'enable_parter' do
+  command "sed -i 's_# deb http://archive\.canonical\.com/ubuntu quantal partner_deb http://archive.canonical.com/ubuntu quantal partner_g' sources.list"
+  cwd '/etc/apt/'
+end
+
+execute 'enable_parter_source' do
+  command "sed -i 's_# deb-src http://archive\.canonical\.com/ubuntu quantal partner_deb-src http://archive.canonical.com/ubuntu quantal partner_g' sources.list"
+  cwd '/etc/apt/'
+end
