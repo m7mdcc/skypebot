6160c27ddc588d5e2098af968930e3c260c41e5c
Shane Auckland <shane.auckland@gmail.com>
2014-02-07 21:33:29 +0000

    Setup launcher icons

--- tools/chef/app-cookbooks/ubuntu-config/recipes/default.rb
+++ tools/chef/app-cookbooks/ubuntu-config/recipes/default.rb
@@ -6,4 +6,6 @@ end
 execute 'enable_parter_source' do
   command "sed -i 's_# deb-src http://archive\.canonical\.com/ubuntu quantal partner_deb-src http://archive.canonical.com/ubuntu quantal partner_g' sources.list"
   cwd '/etc/apt/'
+  notifies :run, 'execute[apt-get update]', :immediately
 end
+
