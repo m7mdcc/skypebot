a040bc33bcf8766cd3c73fb1aa8ea5b88f50820d
Shane Auckland <shane.auckland@gmail.com>
2014-02-11 07:55:17 +0000

    Refactor globals out of Jenkins and Github scripts

--- github.php
+++ github.php
@@ -1,28 +1,44 @@
 <?php
 require_once 'vendor/autoload.php';
+use Inviqa\SkypeEngine;
 
-# Hack to work around strange php issue...
-parse_str(file_get_contents('php://input'), $_REQUEST);
-if (isset($_REQUEST['payload'])) {
-    $payload = json_decode($_REQUEST['payload']);
+$githubHandler = new GithubHandler();
+$githubHandler->handleRequest(file_get_contents('php://input'));
 
-    if ($payload) {
-        $message = array();
-        foreach ($payload->commits as $commit) {
-            $lines = explode("\n", $commit->message);
-            $message[] = '- ' . $lines[0] . ' ('.substr($commit->id, 0, 6).')';
+class GithubHandler
+{
+    public function handleRequest($input)
+    {
+        $this->getRequest($input);
+
+        if (!isset($request['payload'])) {
+            return;
         }
 
-        $info = sprintf(
-            "%s pushed to %s at %s/%s.\n%s",
-            $payload->head_commit->committer->name,
-            $payload->ref,
-            $payload->repository->organization,
-            $payload->repository->name,
-            join("\n", $message)
-        );
+        if ($payload = json_decode($_REQUEST['payload'])) {
+            $message = array();
 
-        Inviqa\SkypeEngine::getDbusProxy()->Invoke( "CHATMESSAGE {$_REQUEST['id']} $info");
+            foreach ($payload->commits as $commit) {
+                $lines = explode("\n", $commit->message);
+                $message[] = '- ' . $lines[0] . ' ('.substr($commit->id, 0, 6).')';
+            }
+
+            $info = sprintf(
+                "%s pushed to %s at %s/%s.\n%s",
+                $payload->head_commit->committer->name,
+                $payload->ref,
+                $payload->repository->organization,
+                $payload->repository->name,
+                join("\n", $message)
+            );
+
+            SkypeEngine::getDbusProxy()->Invoke( "CHATMESSAGE {$_REQUEST['id']} $info");
+        }
+    }
+
+    private function getRequest($input)
+    {
+        parse_str($input, $request);
+        return $request;
     }
 }
-
--- jenkins.php
+++ jenkins.php
@@ -1,89 +1,103 @@
 <?php
 require_once 'vendor/autoload.php';
 
-$payload = json_decode(file_get_contents('php://input'));
-$info = "";
+use Inviqa\SkypeEngine;
 
-if(isset($_REQUEST['filter']) ) {
-    $match = match_filters(
-        (array) $_REQUEST['filter'],
-        $_REQUEST,
-        $payload
-    );
+$jenkinsHandler = new JenkinsHandler();
+$jenkinsHandler->handle(file_get_contents('php://input'));
 
-    if($match !== true) {
-        die($match);
-    }
-}
+class JenkinsHandler
+{
+    public function handle($input)
+    {
+        if (!($payload = json_decode($input))) {
+            return;
+        }
 
-switch($payload->build->phase) {
-    case 'STARTED':
-        $info = sprintf(
-            "%s build %s has started at %s.",
-            $payload->name,
-            $payload->build->number,
-            $payload->build->full_url
-        );
-        break;
-    case 'FINISHED':
-        $info = sprintf(
-            "%s build number %s finished with status %s at %s.",
-            $payload->name,
-            $payload->build->number,
-            $payload->build->status,
-            $payload->build->full_url
-        );
-        break;
-    default:
-        die('Not interested');
-}
+        if(isset($_REQUEST['filter']) ) {
+            $match = $this->match_filters(
+                (array) $_REQUEST['filter'],
+                $_REQUEST,
+                $payload
+            );
 
-Inviqa\SkypeEngine::getDbusProxy()->Invoke( "CHATMESSAGE {$_REQUEST['id']} $info");
-
-/**
- * Determines if requested filters mach payload.
- *
- * Nested fields can be accessed by using a hyphen.
- * e.g. for payload { build: {status: success } }
- * build status can be filtered with "build-status"
- *
- * @param mixed $filters String (single field) or array of fields to filter on.
- * @param array $input Input that specifies filter values.
- * @param stdObject $payload Parsed payload.
- *
- * @return mixed Boolean true if filters are OK, string with failure reason otherwise.
- */
-function match_filters($filters, $input, $payload) {
-    foreach($filters as $filter) {
-        $key = "filter-{$filter}";
-        if(isset($input[$key])) {
-            $value = get_payload_value($filter, $payload);
-            if($input[$key] != $value) {
-                return "'$filter' filter did not match ('{$input[$key]}' != '$value')\n";
+            if($match !== true) {
+                die($match);
             }
         }
-    }
-    return true;
-}
 
-/**
- * Traverses the payload and returns the value specified by path.
- *
- * Nesting is represented by a hyphen.
- *
- * @param string $path Path to field
- * @param stdobject $payload Parsed payload
- * @return mixed Null if path could not be accessed, value of field specified by path otherwise
- */
-function get_payload_value($path, $payload) {
-    $current = $payload;
-    $path = explode('-', $path);
-    foreach($path as $elem) {
-        if(is_object($current) && isset($current->{$elem})) {
-            $current =& $current->{$elem};
-        } else {
-            return null;
+        $info = "";
+
+        switch($payload->build->phase) {
+            case 'STARTED':
+                $info = sprintf(
+                    "%s build %s has started at %s.",
+                    $payload->name,
+                    $payload->build->number,
+                    $payload->build->full_url
+                );
+                break;
+            case 'FINISHED':
+                $info = sprintf(
+                    "%s build number %s finished with status %s at %s.",
+                    $payload->name,
+                    $payload->build->number,
+                    $payload->build->status,
+                    $payload->build->full_url
+                );
+                break;
+            default:
+                die('Not interested');
         }
+
+        SkypeEngine::getDbusProxy()->Invoke( "CHATMESSAGE {$_REQUEST['id']} $info");
     }
-    return $current;
+
+    /**
+     * Determines if requested filters mach payload.
+     *
+     * Nested fields can be accessed by using a hyphen.
+     * e.g. for payload { build: {status: success } }
+     * build status can be filtered with "build-status"
+     *
+     * @param mixed $filters String (single field) or array of fields to filter on.
+     * @param array $input Input that specifies filter values.
+     * @param stdObject $payload Parsed payload.
+     *
+     * @return mixed Boolean true if filters are OK, string with failure reason otherwise.
+     */
+    private function match_filters($filters, $input, $payload) {
+        foreach($filters as $filter) {
+            $key = "filter-{$filter}";
+            if(isset($input[$key])) {
+                $value = $this->get_payload_value($filter, $payload);
+                if($input[$key] != $value) {
+                    return "'$filter' filter did not match ('{$input[$key]}' != '$value')\n";
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Traverses the payload and returns the value specified by path.
+     *
+     * Nesting is represented by a hyphen.
+     *
+     * @param string $path Path to field
+     * @param stdobject $payload Parsed payload
+     * @return mixed Null if path could not be accessed, value of field specified by path otherwise
+     */
+    private function get_payload_value($path, $payload) {
+        $current = $payload;
+        $path = explode('-', $path);
+        foreach($path as $elem) {
+            if(is_object($current) && isset($current->{$elem})) {
+                $current =& $current->{$elem};
+            } else {
+                return null;
+            }
+        }
+        return $current;
+    }
 }
