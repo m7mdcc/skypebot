9fabdd4e848b3c78b802acb7c350f30dfed7a33b
Shane Auckland <shane.auckland@gmail.com>
2014-02-18 07:58:21 +0000

    Reduce chat message handler complexity

--- src/Inviqa/Command/ChatMessageCommandHandler.php
+++ src/Inviqa/Command/ChatMessageCommandHandler.php
@@ -2,6 +2,7 @@
 
 namespace Inviqa\Command;
 
+use Inviqa\Command\ChatMessage\ChatMessageHandlerInterface;
 use Inviqa\SkypeEngine;
 use Inviqa\SkypeCommand;
 use Inviqa\SkypeCommandInterface;
@@ -9,20 +10,20 @@ use Inviqa\SkypeCommandInterface;
 class ChatMessageCommandHandler implements CommandHandlerInterface
 {
     const COMMAND = 'CHATMESSAGE';
-    
+
     protected $engine;
 
     protected $handlers = array();
-    
+
     public function handleCommand(SkypeCommandInterface $command)
     {
         if (self::COMMAND !== $command->getCommand()) {
             return false;
         }
-        
+
         $name = $command->getName();
-        
-        if ($command->getArgument() === 'STATUS' && $command->getValue() === 'RECEIVED') {
+
+        if ($this->isReceivedCommand($command)) {
             $chatname = new SkypeCommand($this->engine->invoke("GET CHATMESSAGE $name CHATNAME"));
             $handle = new SkypeCommand($this->engine->invoke("GET CHATMESSAGE $name FROM_HANDLE"));
             $body = new SkypeCommand($this->engine->invoke("GET CHATMESSAGE $name BODY"));
@@ -33,23 +34,33 @@ class ChatMessageCommandHandler implements CommandHandlerInterface
 //                $body->getValue()
 //            );
 
-            foreach ($this->handlers as $messageHandler) {
-                $messageHandler->handle($chatname, $handle, $body);
-            }
+            $this->triggerHandlers($chatname, $handle, $body);
         }
         $this->engine->invoke("SET CHATMESSAGE $name SEEN");
         return true;
     }
-    
+
     public function setEngine(SkypeEngine $engine)
     {
         $this->engine = $engine;
     }
-    
-    public function add(ChatMessage\ChatMessageHandlerInterface $messageHandler)
+
+    public function add(ChatMessageHandlerInterface $messageHandler)
     {
         $messageHandler->setEngine($this->engine);
         $this->handlers[] = $messageHandler;
         return $this;
+    }
+
+    protected function isReceivedCommand(SkypeCommandInterface $command)
+    {
+        return $command->getArgument() === 'STATUS' && $command->getValue() === 'RECEIVED';
+    }
+
+    protected function triggerHandlers(SkypeCommand $chatname, SkypeCommand $handle, SkypeCommand $body)
+    {
+        foreach ($this->handlers as $messageHandler) {
+            $messageHandler->handle($chatname, $handle, $body);
+        }
     }
 }
